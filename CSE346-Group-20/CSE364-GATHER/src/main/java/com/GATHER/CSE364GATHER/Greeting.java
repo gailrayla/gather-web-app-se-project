caa1ca33068d2e2ef8e66c8944f958f121cf67d6
Gail Rayla Emanuelle Parayno <109154838+gailrayla@users.noreply.github.com>
2023-04-05 22:42:46 +0900

    Add files via upload

---
+++ CSE346-Group-20/CSE364-GATHER/src/main/java/com/GATHER/CSE364GATHER/Greeting.java
@@ -0,0 +1,18 @@
+package com.GATHER.CSE364GATHER;
+
+public class Greeting {
+    private Long id;
+    private String content;
+
+    public Greeting(Long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getContent(){
+        return content;
+    }
+    
+}
