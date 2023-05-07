b54e9c7ad5201ef94567baedfce5bd124a0cbfae
Funda30 <107970477+Funda30@users.noreply.github.com>
2023-05-07 11:14:27 +0900

    Add files via upload

---
+++ milestone2/gather /src/test/java/gather/gather/GatherApplicationTests.java
@@ -0,0 +1,13 @@
+package gather.gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
