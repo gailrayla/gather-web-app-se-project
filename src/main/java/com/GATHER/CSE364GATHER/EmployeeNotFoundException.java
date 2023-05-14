02584950168422b179dd8497b69e5bf5b42e7dd7
Funda30 <107970477+Funda30@users.noreply.github.com>
2023-05-14 22:55:43 +0900

    Delete src directory

--- src/main/java/com/GATHER/CSE364GATHER/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.GATHER.CSE364GATHER;
-
-class EmployeeNotFoundException extends RuntimeException {
-
-    EmployeeNotFoundException(Long id) {
-        super("Could not find employee " + id);
-    }
-}
